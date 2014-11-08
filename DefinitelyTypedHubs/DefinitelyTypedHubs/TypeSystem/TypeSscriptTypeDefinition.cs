a6b64fe5bea74885fb7de350556bb5eaf0abfd14
Bill Wagner <bill.w.wagner@outlook.com>
2014-11-07 18:17:45 -0800

    add definition for type script type definition

---
+++ DefinitelyTypedHubs/DefinitelyTypedHubs/TypeSystem/TypeSscriptTypeDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefinitelyTypedHubs.TypeSystem
+{
+    public class TypeScriptTypeDefinition
+    {
+        public string Name { get; }
+        public string Declaration { get; }
+
+        public TypeScriptTypeDefinition(string name, string declaration)
+        {
+            this.Name = name;
+            this.Declaration = declaration;
+        }
+    }
+}
